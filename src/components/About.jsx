7c67f4df77bb3321b52131ee908b9a350e3e25cb
YassineElkefi <yassine.elkefi6@gmail.com>
2026-04-07 23:17:38 +0100

    Updated Projects count

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import aboutImg from '../assets/about.webp'
 
 const stats = [
   { num: '6+', label: 'Years Coding' },
-  { num: '12+', label: 'Projects Built' },
+  { num: '14+', label: 'Projects Built' },
   { num: '4',  label: 'Internships' },
 ]
 
